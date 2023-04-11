350d30c16113bdb4974659a20c74d440f5d856a3
punk_wei <2420393617@qq.com>
2023-04-12 00:05:59 +0800

    修改了代理文件

--- login.py
+++ login.py
@@ -10,6 +10,10 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62",
 
 }
+getPublicKeyUrl="https://jwxt.stbu.edu.cn/xtgl/login_getPublicKey.html?time=" # 获取公钥
+getocrdddUrl="https://jwxt.stbu.edu.cn/kaptcha?time="
+loginUrl="https://jwxt.stbu.edu.cn/xtgl/login_slogin.html?time="
+getSessinUrl="https://jwxt.stbu.edu.cn/xtgl/login_slogin.html?time="
 def getProxy():
     proxy_list=[]
     with open("./proxy.txt","r",encoding="utf-8") as fp:
@@ -27,18 +31,17 @@ def getPublicKey(session):
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp()*1000))
     end_time= int(round((today.timestamp()+100000)*1000))
-    url="https://jwxt.stbu.edu.cn/xtgl/login_getPublicKey.html?time="+str(start_time)+"&_="+str(end_time)
+    url=getPublicKeyUrl+str(start_time)+"&_="+str(end_time)
     print(url)
     Proxy=getProxy()
     response=session.get(url=url,headers=headers,proxies=Proxy)
     obj =  response.json()
     return obj['modulus'],obj['exponent']
 
-
 def ocrddd(session):
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp()*1000))
-    url="https://jwxt.stbu.edu.cn/kaptcha?time="+str(start_time)
+    url=getocrdddUrl+str(start_time)
     Proxy=getProxy()
     response=session.get(url=url,headers=headers,proxies=Proxy)
     img=response.content
@@ -54,7 +57,7 @@ def jiami(modulus,exponent,psw ):
 def login(yhm,en_pwd,yzm,session):
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp() * 1000))
-    url="https://jwxt.stbu.edu.cn/xtgl/login_slogin.html?time="+str(start_time)
+    url=loginUrl+str(start_time)
     data={
             "csrftoken": "8f8a1149-c13c-48ff-a480-882cc9251325,8f8a1149c13c48ffa480882cc9251325",
             "language": "zh_CN",
@@ -82,7 +85,7 @@ def getSession():
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp() * 1000))
     Proxy=getProxy()
-    url="https://jwxt.stbu.edu.cn/xtgl/login_slogin.html?time="+str(start_time)
+    url=getSessinUrl+str(start_time)
     requests.get(url=url,headers=headers,proxies=Proxy)
     return session
 def start():
