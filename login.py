2a663033b0df4d6e5d58117681ee3f9944956c46
punk_wei <2420393617@qq.com>
2023-04-11 23:58:09 +0800

    修改了代理文件

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 import ddddocr
 from PyRsa.pyb64 import Base64
@@ -9,32 +10,42 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62",
 
 }
-Proxy = {
-        'http': '182.131.17.19:80'
-    }
+def getProxy():
+    proxy_list=[]
+    with open("./proxy.txt","r",encoding="utf-8") as fp:
+        for item in fp:
+            item = eval(item)
+            proxy_list.append(item)
+    if len(proxy_list)>0:
+        Proxy = random.choice(proxy_list)
+    else:
+        Proxy=None
+    return Proxy
+
+
 def getPublicKey(session):
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp()*1000))
     end_time= int(round((today.timestamp()+100000)*1000))
     url="https://jwxt.stbu.edu.cn/xtgl/login_getPublicKey.html?time="+str(start_time)+"&_="+str(end_time)
     print(url)
+    Proxy=getProxy()
     response=session.get(url=url,headers=headers,proxies=Proxy)
     obj =  response.json()
     return obj['modulus'],obj['exponent']
 
 
 def ocrddd(session):
-
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp()*1000))
-    url="https://jwxt.stbu.edu.cn/kaptcha?time"+str(start_time)
+    url="https://jwxt.stbu.edu.cn/kaptcha?time="+str(start_time)
+    Proxy=getProxy()
     response=session.get(url=url,headers=headers,proxies=Proxy)
     img=response.content
     orc=ddddocr.DdddOcr()
     res= orc.classification(img)
     print(res)
     return res
-
 def jiami(modulus,exponent,psw ):
     rsakey = RsaKey()
     rsakey.set_public(Base64().b64tohex(modulus), Base64().b64tohex(exponent))
@@ -52,6 +63,7 @@ def login(yhm,en_pwd,yzm,session):
             "mm":en_pwd,
             "yzm": yzm
     }
+    Proxy=getProxy()
     response=session.post(url=url,headers=headers,data=data,proxies=Proxy)
     html=response.content.decode('utf-8')
     tree=etree.HTML(html)
@@ -69,13 +81,10 @@ def getSession():
     session = requests.session()
     today = datetime.datetime.now()  # 获取今天时间
     start_time = int(round(today.timestamp() * 1000))
+    Proxy=getProxy()
     url="https://jwxt.stbu.edu.cn/xtgl/login_slogin.html?time="+str(start_time)
     requests.get(url=url,headers=headers,proxies=Proxy)
-
     return session
-
-
-
 def start():
     # 1.准备session
     session = getSession()
